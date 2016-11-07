5a30c438c2c7bc28b4ef1d842c07f5a6fa98858b
doesmsche <doemsche@gmx.ch>
2016-11-07 22:20:16 +0100

    3rd commit

--- test_db_es6.js
+++ test_db_es6.js
@@ -2,13 +2,17 @@ import Sequelize from 'sequelize';
 import _ from 'underscore';
 import Faker from 'faker';
 
-const sequelize = new Sequelize('myDB','dschlaepfer','', {
+const Conn = new Sequelize('myDB','dschlaepfer','', {
   host: 'localhost',
   dialect: 'sqlite',
   storage: './database.sqlite'
 });
 
-const User = sequelize.define('user', {
+const User = Conn.define('user', {
+  // uuid: {
+  //     type: Sequelize.UUID,
+  //     primaryKey: true
+  //   },
   firstName: {
     type: Sequelize.STRING,
     field: 'first_name', // Will result in an attribute that is firstName when user facing but first_name in the database
@@ -22,22 +26,45 @@ const User = sequelize.define('user', {
 }
 );
 
+const Group = Conn.define('group',{
+  // uuid: {
+  //     type: Sequelize.UUID,
+  //     primaryKey: true
+  //   },
+  groupName: {
+    type: Sequelize.STRING
+  }
+});
+
+User.belongsTo(Group,{foreignKey: 'group_id'})
+Group.hasMany(User);
 
 
-if(process.argv[2] == '--rebuild'){
-  User.sync({force: true}).then(() => {
-    // Table created
-    _.times(109, ()=>{
-      return User.create({
-        firstName: Faker.name.firstName(),
-        lastName: Faker.name.lastName()
+// if(process.argv[2] == '--rebuild'){
+  Conn.sync({force: true}).then(() => {
+    _.times(10,()=>{
+      return Group.create({
+        uuid: Faker.random.uuid(),
+        groupName: Faker.company.companyName()
+      }).then(group =>{
+        _.times(Math.floor((Math.random() * 10) + 0), ()=>{
+          return User.create({
+            uuid: Faker.random.uuid(),
+            group_id:  group.id,
+            firstName: Faker.name.firstName(),
+            lastName: Faker.name.lastName()
+          });
+        })
       });
-    })
+    });
   });
-}
-
-if(process.argv[2] != '--rebuild'){
-  User.findOne().then((user)=> {
-      console.log(user.get('firstName'));
-  });
-}
+// }
+      // Group.findOne().then(function (g) {
+      //     console.log(g.get('uuid'));
+      //     console.log(g.get('groupName'));
+      // });  
+// if(process.argv[2] != '--rebuild'){
+//   User.findOne().then((user)=> {
+//       console.log(user.get('firstName'));
+//   });
+// }
